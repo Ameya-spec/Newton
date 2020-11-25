5a5e028cce1be4452db8379b50f8bccb8bd04ef2
Ameya-spec <70754404+Ameya-spec@users.noreply.github.com>
2020-11-25 16:00:25 +0530

    Add files via upload

---
+++ rect.js
@@ -0,0 +1,24 @@
+class Rect{
+constructor(x,y,width,height){
+var options = {
+    restitution:0.8,
+    friction:1.0,
+    density:1.0
+}
+this.body = Bodies.rectangle(x, y, width, height, options);
+this.width = width;
+this.height = height;
+
+World.add(world,this.body);
+}
+display(){
+    var angle = this.body.angle;
+    var pos = this.body.position;
+    pos.x=mouseX,pos.y=mouseY
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    rotate(angle);
+    fill("black")
+    pop();
+}
+}
